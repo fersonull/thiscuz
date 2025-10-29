013adce99ff9315faf81cf03e2fc2626c9c72fa7
mrfool <mrfool@tarot.club>
2025-10-29 23:15:50 +0800

    create navbar component

---
+++ resources/js/components/layout/navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "@inertiajs/react"
+
+export default function navbar() {
+    return (
+        <div className="flex items-center justify-center lg:px-16 py-3">
+            <div className="flex items-center justify-between w-full">
+                <Link href="/">
+                    <span className="tracking-tigher font-medium">thiscuz</span>
+                </Link>
+            </div>
+        </div>
+    )
+}
