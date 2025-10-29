8cc910230f456453976902ebf0867dd7dacd7780
mrfool <mrfool@tarot.club>
2025-10-30 00:02:05 +0800

    update the navbar

--- resources/js/components/layout/navbar.jsx
+++ resources/js/components/layout/navbar.jsx
@@ -1,12 +1,21 @@
 import { Link } from "@inertiajs/react"
+import { LogIn } from "lucide-react"
+import { Button } from "../ui/button"
 
 export default function Navbar() {
     return (
-        <div className="flex items-center justify-center lg:px-16 py-3">
-            <div className="flex items-center justify-between w-full">
+        <div className="flex items-center justify-center py-3">
+            <div className="flex items-center justify-between w-full lg:max-w-4xl xl:max-w-6xl max-w-md md:max-w-3xl">
                 <Link href="/">
-                    <span className="tracking-tigher font-medium">thiscuz</span>
+                    <span className="tracking-tigher font-bold">thiscuz</span>
                 </Link>
+
+                <div className="flex gap-2">
+                    <Button size="sm">
+                        <LogIn />
+                        Login
+                    </Button>
+                </div>
             </div>
         </div>
     )
