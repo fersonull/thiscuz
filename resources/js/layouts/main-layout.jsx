ad3b7d3cd03d707c376f061a1e318075b86b666d
mrfool <mrfool@tarot.club>
2025-10-30 14:44:47 +0800

    change the main font to geist

--- resources/js/layouts/main-layout.jsx
+++ resources/js/layouts/main-layout.jsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/layout/navbar"
 
 export default function MainLayout({ children }) {
     return (
-        <div className="flex flex-col h-screen">
+        <div className="flex flex-col h-screen font-geist">
             <Navbar />
 
             <main className="flex-1 lg:max-w-4xl xl:max-w-6xl max-w-md md:max-w-3xl w-full mx-auto">
