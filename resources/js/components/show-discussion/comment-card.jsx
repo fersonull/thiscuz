c17e9941b5ed35318a09a529e331fd1d4639023c
mrfool <mrfool@tarot.club>
2025-10-31 14:06:37 +0800

    add time and dummy data for comments

--- resources/js/components/show-discussion/comment-card.jsx
+++ resources/js/components/show-discussion/comment-card.jsx
@@ -3,19 +3,22 @@ import { Avatar, AvatarImage } from "../ui/avatar"
 import { Button } from "../ui/button"
 import CommentBubble from "./comment-bubble"
 
-export default function CommentCard() {
+export default function CommentCard({ name, content }) {
     return (
-        <div className='flex gap-3 relative group'>
+        <div className='flex gap-3 relative group mb-3'>
             <Avatar>
                 <AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500" />
             </Avatar>
 
             <div className="flex flex-col group">
-                <p className="mb-1 font-medium">Jasfer Monton</p>
+                <div className="flex items-center gap-2">
+                    <p className="mb-1 font-medium">{name}</p>
+                    <p className="text-sm text-muted-foreground"> 3 seconds ago</p>
+                </div>
 
-                <CommentBubble content="just install it on vms. it will just install a windows os on that vm and then you can play any game with anti cheat" />
+                <CommentBubble content={content} />
 
-                <div className="w-full mt-1 relative lg:hidden text-end lg:group-hover:block">
+                <div className="w-full mt-1 relative lg:hidden lg:group-hover:block">
                     <Button variant="outline" className="right-0 border-0 shadow-none">
                         <MessagesSquare />
                         Reply
--- resources/js/components/show-discussion/comments.jsx
+++ resources/js/components/show-discussion/comments.jsx
@@ -1,12 +1,21 @@
 import CommentCard from "./comment-card"
 
+const comments = [
+    { name: 'Jasfer Monton', comment: "just install it on vms. it will just install a windows os on that vm and then you can play any game with anti cheat" },
+    { name: 'Kimberly Macatangay', comment: "idk man. just use windows" },
+    { name: 'Json Api', comment: "use something like vm bro, it always works for mes" },
+    { name: 'Kimberly Macatangay', comment: "hi" },
+]
+
 export default function Comments() {
     return (
         <div>
             <p><span className='font-semibold'>12</span> Replies</p>
 
             <div className="w-full mt-4">
-                <CommentCard />
+                {comments.map(({ name, comment }) => (
+                    <CommentCard name={name} content={comment} />
+                ))}
             </div>
         </div>
     )
