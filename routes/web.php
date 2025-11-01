aca8d9d37322949b6548b1e7d885e73780292238
mrfool <mrfool@tarot.club>
2025-11-01 14:27:39 +0800

    add blank page for login and register

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::inertia("/", "home");
 Route::inertia("/discussions", "discussions/index")->name("discussions");
 Route::inertia("/discussions/{id}", "show-discussion/index");
+
+Route::inertia("/login", "auth/login-page")->name("login");
