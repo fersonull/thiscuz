60b42bc52f92657c7fda7dcf0ba19ce151e232cc
fersonull <culaspero@gmail.com>
2025-11-06 10:45:15 +0800

    add route for regsiter page

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ Route::inertia("/discussions", "discussions/index")->name("discussions");
 Route::inertia("/discussions/{id}", "show-discussion/index");
 
 Route::inertia("/login", "auth/login-page")->name("login");
+Route::inertia("/register", "auth/register-page")->name("register");
